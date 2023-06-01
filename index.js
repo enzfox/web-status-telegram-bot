899f680f77e9c964c2a4300870ea51cef69c747e
alexx <cristea.alecs@gmail.com>
2023-06-01 05:22:06 +0300

    app: removed cron, replaced by setInterval

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 import TelegramBot from "node-telegram-bot-api";
 import get from "axios";
-import { schedule } from "node-cron";
 import dotenv from "dotenv";
 
-// Configure environment variables
 dotenv.config();
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const chatId = process.env.TELEGRAM_CHAT_ID;
-const websites = process.env.WEBSITES.split(",");
+const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, WEBSITES } = process.env;
 
-// Setup Telegram bot
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
-// Create an array of objects to store website information
-const websiteStatus = websites.map((website) => ({
-  url: website,
+const websiteStatus = WEBSITES.split(",").map((url) => ({
+  url,
   isUp: true,
   lastNotification: null,
   downSince: null,
 }));
 
-// Check if a website is up or down
 async function checkWebsiteStatus(url) {
   try {
     const response = await get(url);
@@ -31,56 +24,45 @@ async function checkWebsiteStatus(url) {
   }
 }
 
-// Schedule the website checker to run every minute (use a different cron pattern if needed)
-schedule("* * * * *", async () => {
-  // Get current time
-  const currentTime = new Date().getTime(); // Use getTime() to get time in milliseconds
+setInterval(async () => {
+  const currentTime = new Date();
 
-  // Iterate through each website
   for (const website of websiteStatus) {
-    // Check if the website is up or down
     const isUp = await checkWebsiteStatus(website.url);
+    const { isUp: wasUp, lastNotification, downSince } = website;
 
-    // Calculate time since last notification (if any in last 10 minutes then don't send notification)
-    const timeSinceLastNotification = website.lastNotification
-      ? (currentTime - website.lastNotification) / 1000 / 60 // Convert to minutes
+    const timeSinceLastNotification = lastNotification
+      ? (currentTime - lastNotification) / 1000 / 60
       : null;
 
-    // Check if the website is down and if it's been more than 10 minutes since the last notification
-    if (!isUp && (website.isUp || timeSinceLastNotification >= 10)) {
-      // Check again this website
-      if (!(await checkWebsiteStatus(website.url))) {
-        // Send a message to your Telegram chat when a website is down
-        bot.sendMessage(
-          chatId,
-          `⚠️ The website ${website.url} appears to be down.`
-        );
+    const isNotificationNeeded =
+      !isUp && (wasUp || timeSinceLastNotification >= 10);
+    const isRecoveryNotificationNeeded = isUp && !wasUp && downSince !== null;
 
-        // Update website last notification time
-        website.lastNotification = currentTime;
-
-        // Set downSince if it's not already set
-        if (!website.downSince) {
-          website.downSince = currentTime;
-        }
-      }
-    } else if (isUp && !website.isUp) {
-      // Calculate downtime duration
-      const downtimeDuration = Math.floor(
-        (currentTime - website.downSince) / 1000 / 60
-      ); // Convert to minutes
-
-      // Send a message to your Telegram chat when a website is back up
+    if (isNotificationNeeded) {
       bot.sendMessage(
-        chatId,
+        TELEGRAM_CHAT_ID,
+        `⚠️ The website ${website.url} appears to be down.`
+      );
+
+      website.lastNotification = currentTime;
+
+      if (downSince === null) {
+        website.downSince = currentTime;
+      }
+    } else if (isRecoveryNotificationNeeded) {
+      const downtimeDuration = Math.floor(
+        (currentTime - downSince) / 1000 / 60
+      );
+
+      bot.sendMessage(
+        TELEGRAM_CHAT_ID,
         `✅ The website ${website.url} is back up. ⏰ It was down for approximately ${downtimeDuration} minutes.`
       );
 
-      // Update website downtime status
       website.downSince = null;
     }
 
-    // Update website status
     website.isUp = isUp;
   }
-});
+}, 60000);
