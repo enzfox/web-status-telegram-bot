fc49f9ba60b9c8f2b236615e0431b81ce4f3f9c1
alexx <als698@users.noreply.github.com>
2024-06-11 15:38:54 +0300

    fe: added pwa

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      manifest: {
+        name: "WebStatus",
+        short_name: "WebStatus",
+        theme_color: "#e9e1fe",
+        icons: [
+          {
+            src: "/assets/icon/favicon.png",
+            sizes:
+              "16x16 24x24 32x32 64x64 72x72 96x96 128x128 144x144 152x152 192x192 384x384 512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        background_color: "#f5f5f5",
+      },
+    }),
+  ],
+});
