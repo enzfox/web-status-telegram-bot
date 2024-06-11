e033a002d1f383e101799e6d66e1cbeb278edfd8
alexx <als698@users.noreply.github.com>
2024-06-11 09:17:25 +0300

    fe: timeUtils.tsx

---
+++ fe/src/utils/timeUtils.tsx
@@ -0,0 +1,13 @@
+export function formatDownTime(downTime: number): string {
+  const downTimeHours = Math.floor(downTime / 3600)
+    .toString()
+    .padStart(2, "0");
+  const downTimeMinutes = Math.floor((downTime % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const downTimeSeconds = Math.floor(downTime % 60)
+    .toString()
+    .padStart(2, "0");
+
+  return `${downTimeHours}:${downTimeMinutes}:${downTimeSeconds}`;
+}
